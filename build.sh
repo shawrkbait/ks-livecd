9b03eefb14c502d673ec7a8edb8c1bad936aa619
shawrkbait <sjohnson@axiomega.com>
2018-10-02 13:24:14 -0500

    Update image name in build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 iso_name=$1
-docker run -it --privileged --rm -v /tmp:/tmp -v $(pwd):/build ks-livecd livecd-creator -c ks-live.cfg --cache=/tmp/centcache -f $iso_name
+docker run -it --privileged --rm -v /tmp:/tmp -v $(pwd):/build shawrkbait/ks-livecd livecd-creator -c ks-live.cfg --cache=/tmp/centcache -f $iso_name
