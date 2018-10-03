4c7d9af8600135df4dbdf99feabdde07aebb83ff
shawrkbait <sjohnson@axiomega.com>
2018-10-03 14:46:57 -0500

    Remove --privileged from docker cmd

--- build_live.sh
+++ build_live.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -e
 iso_name=$1
-docker run -it --privileged --rm -v /tmp/centcache:/tmp/centcache -v $(pwd):/build shawrkbait/ks-livecd $iso_name --cache=/tmp/centcache
+for f in /dev/loop[0-9]*; do
+  loops="$loops --device ${f}"
+done
+docker run -it --cap-add=SYS_ADMIN $loops --rm -v /tmp/centcache:/tmp/centcache -v $(pwd):/build shawrkbait/ks-livecd $iso_name --cache=/tmp/centcache
 [ -f ${iso_name}.iso ]
