9b0655e0cfa3aff8e5f799d93a2b6fc4a37b5c89
Shawn Johnson <sjohnson@axiomega.com>
2018-10-02 21:05:57 -0500

    Fix travis ci

--- build_live.sh
+++ build_live.sh
@@ -1,3 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 iso_name=$1
 docker run -it --privileged --rm -v /tmp/centcache:/tmp/centcache -v $(pwd):/build shawrkbait/ks-livecd $iso_name --cache=/tmp/centcache
+[ -f ${iso_name}.iso ]
